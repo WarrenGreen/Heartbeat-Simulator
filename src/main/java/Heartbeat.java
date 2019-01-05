47454f26758ef2a5d9a0313d4e88a46221d27ead
Warren Green <s.green.warren@gmail.com>
2019-01-05 12:02:14 -0800

    Rusty java skillz

--- src/main/java/Heartbeat.java
+++ src/main/java/Heartbeat.java
@@ -109,7 +109,7 @@ public class Heartbeat extends JFrame{
   }
 
   public static void main(String[] args) {
-    if (args.length() < 2) {
+    if (args.length < 2) {
       System.out.println("Format: java Heartbeat <width> <height>");
       return;
     }
